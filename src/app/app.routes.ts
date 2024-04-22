7ddb84e956d155ac9a4c527bbe505c5a8ffa1207
Sean Haboon <116582145+shaboon@users.noreply.github.com>
2024-04-22 15:58:19 -0400

    Tested Generating a Component by name
    used the command
    "ng generate component test"
    
    to generate structure of test component
    Can be further expanded upon to experiment with, still need to figure out functionality and how to access these components properly

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from "./test/test.component"
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'TestComponent', component: TestComponent}
+];
